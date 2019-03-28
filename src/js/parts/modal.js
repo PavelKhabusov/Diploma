92cd38ca71fd8439beb3202320f963419d59ab83
Pavel <xabusva20@gmail.com>
2019-03-28 15:01:06 +1000

    Modal

--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -3,23 +3,19 @@ function modal() {
   // -----------
 	// ----- Modal
 	// -----------
-	let more = document.querySelectorAll('.more, .description-btn'),
-  overlay = document.querySelector('.overlay'),
-  close = document.querySelector('.popup-close');
+	let more = document.querySelectorAll('.popup_engineer_btn'),
+  overlay = document.querySelector('.popup_engineer'),
+  close = overlay.querySelector('.popup_close');
 
   more.forEach(function(item) {
     item.addEventListener('click', function() {
       overlay.style.display = 'block';
-      this.classList.add('more-splash');
       document.body.style.overflow = 'hidden';
     });
   });
 
   close.addEventListener('click', function() {
     overlay.style.display = 'none';
-    more.forEach(function(item) {
-      item.classList.remove('more-splash');
-    });
     document.body.style.overflow = '';
   });
 }
